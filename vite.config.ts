9a147852b0e13af35b9ad75388ecb495d5c6553e
rana22 <ambar.rana123@gmail.com>
2025-11-12 12:18:53 -0500

    set root path

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
+const raw = process.env.BASE_PATH ?? '';
+const base = raw.endsWith('/') ? raw : raw + '/';   // force trailing slash
+
 export default defineConfig(
 { 
 	plugins: [svelte()], 
-	base: process.env.BASE_PATH ?? '' 
+	base
 });
